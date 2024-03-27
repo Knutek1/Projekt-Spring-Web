3837887f66e799eb29580fec7b701adfd0c2f26d
Knutek1 <piotr.knuta@gmail.com>
2024-03-27 15:50:14 +0100

    22.2 Zadanie: ułożenie kodu

---
+++ src/main/java/com/crud/tasks/controller/TrelloController.java
@@ -0,0 +1,34 @@
+package com.crud.tasks.controller;
+
+import com.crud.tasks.domain.TrelloBoardDto;
+import com.crud.tasks.trello.client.TrelloClient;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.constraints.NotNull;
+import java.util.List;
+import java.util.Objects;
+
+@RestController
+@RequestMapping("v1/trello")
+@RequiredArgsConstructor
+@CrossOrigin("*")
+public class TrelloController {
+
+    private final TrelloClient trelloClient;
+
+    @GetMapping("boards")
+    public void getTrelloBoards() {
+
+        List<TrelloBoardDto> trelloBoards = trelloClient.getTrelloBoards();
+
+        trelloBoards.stream()
+                .filter(b -> Objects.nonNull(b.getId()) && Objects.nonNull(b.getName()))
+                .filter(b -> b.getName().contains("Kodilla"))
+                .forEach(System.out::println);
+
+        }
+    }
